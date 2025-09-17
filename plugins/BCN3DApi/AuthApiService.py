8739dfb0e8d3a3d2c05ef9eb22f598f54ef7a9fb
Alfonsopl <aplanas@bcn3d.com>
2025-09-17 13:10:42 +0200

    fix error with advanced user

--- plugins/BCN3DApi/AuthApiService.py
+++ plugins/BCN3DApi/AuthApiService.py
@@ -78,10 +78,9 @@ class AuthApiService(QObject):
             self._profile = {}
             self._profile["username"] = current_user["name"]
             self._profile["advanced_user"] = False
-            if "advanced_user" in current_user:
-                advanced_user = current_user["advanced_user"]
+            if "stratos_advanced_user" in current_user:
+                advanced_user = current_user["stratos_advanced_user"]
                 if advanced_user is not None:
-                    advanced_user = json.loads(advanced_user)
                     if "stratos_show_custom" in advanced_user:
                         self._profile["advanced_user"] = advanced_user["stratos_show_custom"]
             self._profile
